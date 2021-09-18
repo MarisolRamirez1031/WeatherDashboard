03c46c5ab6a137bacd357bcdd0c78477099cad76
Marisol Bennett <marisol_ramirez21@live.com>
2021-09-18 13:34:21 -0500

    working on adding to index.html and starting js file

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+var getUserWeather = function() {
+    fetch('https://api.openweathermap.org/data/2.5/onecall?lat=30.17&lon=-97.30&units=imperial&appid=194f452e9fcc6891d991dfc10caa1c42');
+};
+
+getUserWeather();
+
+// function to execute when new city is searched
+
+
+// get current city weather
+
+
+// get forcast and uv index
+
