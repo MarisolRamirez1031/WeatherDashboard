ea3282b84fb86ec82f18d4d6c4e974d55efacd98
Marisol Bennett <marisol_ramirez21@live.com>
2022-01-27 00:39:48 -0600

    setting up endpoint

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,14 @@
-// get current city weather
+const apiKey = "194f452e9fcc6891d991dfc10caa1c42";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
+const currentCity = document.querySelector("#currentWeather")
+
 // api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}
+// get current city weather
+
+let lastCall = null;
+const displayCurrentWeather = function(cityName, date, time, wind, humidity, uv) {
+    ;
+}
 
 var getCurrentCity = function(city) {
     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=194f452e9fcc6891d991dfc10caa1c42";
